8878553e45bda99f3425cde466d901f40d462993
Cesar David Millan <cesardavidmillan5@gmail.com>
2024-04-02 20:01:48 -0500

    Section 2 Completed

--- Section_2_Introduction_To_Reactive_Programming/reactive-examples/src/main/java/guru/springframework/reactiveexamples/PersonRepositoryImpl.java
+++ Section_2_Introduction_To_Reactive_Programming/reactive-examples/src/main/java/guru/springframework/reactiveexamples/PersonRepositoryImpl.java
@@ -5,13 +5,19 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class PersonRepositoryImpl implements PersonRepository {
+
+    Person michael = new Person(1,"Michael", "Weston");
+    Person fiona = new Person(2,"Fiona","Glenanne");
+    Person sam = new Person(3,"Sam","Axe");
+    Person jesse = new Person(4, "Jesse","Porter");
+
     @Override
     public Mono<Person> getById(Integer id) {
-        return null;
+        return findAll().filter(person -> person.getId() == id).next();
     }
 
     @Override
     public Flux<Person> findAll() {
-        return null;
+        return Flux.just(michael,fiona,sam,jesse);
     }
 }
